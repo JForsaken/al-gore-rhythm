5151e7de462d5c4d7ad3dbe376565c411b745bae
Francis <francis.beauchampsc@gmail.com>
2016-11-12 15:04:03 -0500

    Fixes lint

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
 import getData from './lector';
 
-const data = getData('CSV');
-console.log(data);
+getData('CSV');
--- src/lector.js
+++ src/lector.js
@@ -4,24 +4,67 @@ import csvData from '../assets/data.csv';
 
 function transform(data) {
   const keys = data[0];
-  const values = data.slice(1);
+  const values = data.slice(1).filter((datum) => {
+    return datum.length > 1;
+  }); // The last data is of length 1 for some reasons
 
-  function buildPlayerFromDatum(playerData) {
+  const uniqueIds = [];
+
+  function buildPlayerFromDatum(playerDatum) {
     const player = {};
-    playerData.forEach((datum, index) => {
+    playerDatum.forEach((datum, index) => {
       const key = keys[index];
-      player[key] = parseFloat(datum); // Data should only contain float & integer values
+
+      // Data should only contain float & integer values
+      player[key] = parseFloat(datum) || 0;
     });
 
     return player;
   }
 
-  const playerObjects = values.map(buildPlayerFromDatum);
-  return playerObjects;
-}
+  function normalize(datum) {
+    // Ensure league is valid
+    const isLeagueValid = datum.LeagueIndex >= 1 && datum.LeagueIndex <= 8;
+    const isUnique = !uniqueIds.includes(datum.GameID); // Remove duplicates gamer ID
 
-function normalize(datum) {
+    uniqueIds.push(datum.GameID);
+    return isLeagueValid && isUnique;
+  }
 
+  function reduceComplexity(datum) {
+    return datum; // TODO Choose what datum keys we remove to reduce the complexity
+  }
+
+  function computeAverageValues(playerData) {
+    const reduced = playerData.reduce((prev, cur) => {
+      Object.keys(cur).forEach((key) => {
+        if (!(key in prev)) {
+          prev[key] = 0.0;
+        }
+
+        if (!(key in cur)) {
+          cur[key] = 0.0;
+        }
+
+        cur[key] += prev[key];
+      });
+
+      return cur;
+    }, {});
+
+    const average = {};
+    Object.keys(reduced).forEach((key) => {
+      average[key] = reduced[key] / playerData.length;
+    });
+
+    return average;
+  }
+
+  const playerObjects = values.map(buildPlayerFromDatum).map(reduceComplexity);
+  // compute average values;
+  const averageValues = computeAverageValues(playerObjects);
+  console.log(averageValues);
+  return playerObjects.filter(normalize);
 }
 
 // testing read file of JSON and CSV
