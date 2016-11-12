f4bed4d54ae97ff452d0221d94974df66ad574f0
Francis <francis.beauchampsc@gmail.com>
2016-11-12 12:58:56 -0500

    Moves the reading code to lector

--- src/index.js
+++ src/index.js
@@ -1,18 +1,4 @@
 import getData from './lector';
 
 const data = getData('CSV');
-const keys = data[0];
-const values = data.slice(1);
-
-function buildPlayerFromDatum(playerData) {
-  const player = {};
-  playerData.forEach((datum, index) => {
-    const key = keys[index];
-    player[key] = parseFloat(datum); // Data should only contain float & integer values
-  });
-
-  return player;
-}
-
-const playerObjects = values.map(buildPlayerFromDatum);
-console.log(playerObjects);
+console.log(data);
--- src/lector.js
+++ src/lector.js
@@ -2,6 +2,28 @@
 import jsonData from '../assets/data.json';
 import csvData from '../assets/data.csv';
 
+function transform(data) {
+  const keys = data[0];
+  const values = data.slice(1);
+
+  function buildPlayerFromDatum(playerData) {
+    const player = {};
+    playerData.forEach((datum, index) => {
+      const key = keys[index];
+      player[key] = parseFloat(datum); // Data should only contain float & integer values
+    });
+
+    return player;
+  }
+
+  const playerObjects = values.map(buildPlayerFromDatum);
+  return playerObjects;
+}
+
+function normalize(datum) {
+
+}
+
 // testing read file of JSON and CSV
 // Also tests webpack's CSV to JSON conversion
 export default (type) => {
@@ -9,5 +31,5 @@ export default (type) => {
     return jsonData;
   }
 
-  return csvData;
+  return transform(csvData);
 };
