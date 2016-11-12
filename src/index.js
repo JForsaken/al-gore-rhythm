d22e7cc77fe148ac65e594a611f8e6bd2b363f6c
Francis <francis.beauchampsc@gmail.com>
2016-11-12 12:48:13 -0500

    Adds associative array with float values

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 import getData from './lector';
 
-console.log(getData('JSON'));
+const data = getData('CSV');
+const keys = data[0];
+const values = data.slice(1);
+
+function buildPlayerFromDatum(playerData) {
+  const player = {};
+  playerData.forEach((datum, index) => {
+    const key = keys[index];
+    player[key] = parseFloat(datum); // Data should only contain float & integer values
+  });
+
+  return player;
+}
+
+// An item should be a list of values
+function parseCSVItem(item) {
+  return buildPlayerFromDatum(item);
+}
+
+const playerObjects = values.map(parseCSVItem);
+console.log(playerObjects);
