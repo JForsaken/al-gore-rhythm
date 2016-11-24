43f9ef6d4f51a32bb33f323c4c5b2d7cdda7b84a
Francis <francis.beauchampsc@gmail.com>
2016-11-24 18:32:52 -0500

    merges with master

--- src/lector.js
+++ src/lector.js
@@ -67,42 +67,45 @@ function transform(data) {
     return average;
   }
 
-  const playerObjects = values.map(buildPlayerFromDatum).map(reduceComplexity).filter(removeAberrant).map(normalize);
+  const playerObjects = values.map(buildPlayerFromDatum)
+                              .map(reduceComplexity)
+                              .filter(removeAberrant)
+                              .map(normalize);
 
   // compute average values;
   const averageValues = computeAverageValues(playerObjects);
   console.log('\n' + playerObjects.length + ' ===> AVERAGE VALUES FOR ALL LEAGUES v\n');
   console.log(averageValues);
 
-  const league1Players = playerObjects.filter((player) => { return player.LeagueIndex == 1; });
+  const league1Players = playerObjects.filter((player) => { return player.LeagueIndex === 1; });
   console.log('\n' + league1Players.length + ' ===> AVERAGE VALUES FOR LEAGUE 1 v\n');
   console.log(computeAverageValues(league1Players));
 
-  const league2Players = playerObjects.filter((player) => { return player.LeagueIndex == 2; });
+  const league2Players = playerObjects.filter((player) => { return player.LeagueIndex === 2; });
   console.log('\n' + league2Players.length + ' ===> AVERAGE VALUES FOR LEAGUE 2 v\n');
   console.log(computeAverageValues(league2Players));
-  
-  const league3Players = playerObjects.filter((player) => { return player.LeagueIndex == 3; });
+
+  const league3Players = playerObjects.filter((player) => { return player.LeagueIndex === 3; });
   console.log('\n' + league3Players.length + ' ===> AVERAGE VALUES FOR LEAGUE 3 v\n');
   console.log(computeAverageValues(league3Players));
 
-  const league4Players = playerObjects.filter((player) => { return player.LeagueIndex == 4; });
+  const league4Players = playerObjects.filter((player) => { return player.LeagueIndex === 4; });
   console.log('\n' + league4Players.length + ' ===> AVERAGE VALUES FOR LEAGUE 4 v\n');
   console.log(computeAverageValues(league4Players));
 
-  const league5Players = playerObjects.filter((player) => { return player.LeagueIndex == 5; });
+  const league5Players = playerObjects.filter((player) => { return player.LeagueIndex === 5; });
   console.log('\n' + league5Players.length + ' ===> AVERAGE VALUES FOR LEAGUE 5 v\n');
   console.log(computeAverageValues(league5Players));
 
-  const league6Players = playerObjects.filter((player) => { return player.LeagueIndex == 6; });
+  const league6Players = playerObjects.filter((player) => { return player.LeagueIndex === 6; });
   console.log('\n' + league6Players.length + ' ===> AVERAGE VALUES FOR LEAGUE 6 v\n');
   console.log(computeAverageValues(league6Players));
 
-  const league7Players = playerObjects.filter((player) => { return player.LeagueIndex == 7; });
+  const league7Players = playerObjects.filter((player) => { return player.LeagueIndex === 7; });
   console.log('\n' + league7Players.length + ' ===> AVERAGE VALUES FOR LEAGUE 7 v\n');
   console.log(computeAverageValues(league7Players));
 
-  const league8Players = playerObjects.filter((player) => { return player.LeagueIndex == 8; });
+  const league8Players = playerObjects.filter((player) => { return player.LeagueIndex === 8; });
   console.log('\n' + league8Players.length + ' ===> AVERAGE VALUES FOR LEAGUE 8 v\n');
   console.log(computeAverageValues(league8Players));
 
